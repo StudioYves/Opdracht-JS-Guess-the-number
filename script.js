d990ebb80644e0da66dbef65602346bc02fb1245
Ivo Kersten <82444976+StudioYves@users.noreply.github.com>
2021-06-10 10:41:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ console.log(naam);
 // COMPUTER KIES RANDOM NUMMER
 const min = 1;
 const max = 25;
-
 let computedNumber = function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
+};
 const numberToGuess = computedNumber(min, max);
-console.log(computedNumber(min, max));
+console.log(numberToGuess);
 
 //TE RADEN NUMMER INVOEREN
 let guess = prompt("Hey " + naam + ", voer een nummer in tussen 1 en 25.");
@@ -18,18 +16,18 @@ console.log(guess);
 
 //Checken of nummers overeenkomen
 let checkNumber = function(guess, numberToGuess) {
-    if(guess != numberToGuess) {
-        alert("Helaas, probeer het nog een keer.")
+    if (guess != numberToGuess) {
+        alert("Helaas, dat is niet correct. Probeer het nog een keer.")
+        let guess = prompt("Voer nogmaals een nummer in tussen 1 en 25.");
+        checkNumber(guess, numberToGuess);
     }
     else {
         alert("Gefeliciteerd, je hebt gewonnen!")
+        alert("Dag " + naam + ". Tot de volgende keer!")
     }
-};
+}
 
-let result = checkNumber(guess, numberToGuess);
-console.log(result);
-
-
+checkNumber(guess, numberToGuess);
 
 
 
